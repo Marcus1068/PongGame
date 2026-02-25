77efb2bf6ec81a3f5f85d283393236c3882b869e
Marcus Deuss <marcus.deuss@me.com>
2026-02-25 11:54:15 +0100

    Initial Commit

---
+++ PongGame/PongGameApp.swift
@@ -0,0 +1,32 @@
+//
+//  PongGameApp.swift
+//  PongGame
+//
+//  Created by Marcus Deuß on 25.02.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PongGameApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
